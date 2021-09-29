c6d6ba56c00b4128393b4564a56c263e34abf9b9
Alena Rehberger <alenarehberger92@gmail.com>
2021-09-29 09:07:03 -0500

    Plan for commentRoutes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,4 +1,17 @@
 const router = require('express').Router();
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
+router.gert('/', (req, res) => {
+    Comment.findAll({})
+})
+
+router.post('/', withAuth, (req, res) => {
+
+})
+
+router.delete('/:id', withAuth, (req, res) => {
+    
+})
 
 module.exports = router;
