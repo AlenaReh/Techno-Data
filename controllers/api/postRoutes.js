59461067189f1683874f28404a68c6442ef9b749
Alena Rehberger <alenarehberger92@gmail.com>
2021-09-29 09:03:33 -0500

    Working on the routs

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,34 +1,59 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { Post, User,  Comment } = require('../../models');
+//req my middleware
+const withAuth = require('../../utils/auth');
+const sequelize = require('../../config/connection');
 
-router.post('/', async (req, res) => {
+//all of the posts
+router.get('/', (req, res) => {
+  Post.findAll({
+
+  })
+})
+
+//find individual posts
+router.get('/id', (req, res) => {
+  Post.findOne ({
+
+  })
+})
+
+//create a new post
+// router.post('/', withAuth, (req, res) => {
+//   Post.create({
+//     title: req.body.title,
+//     text: req.body.text,
+//     user_id: req.body.user_id 
+//   })
+//     .then(postData => res.json(postData))
+
+//     .catch(err => {
+//     res.status(400).json(err);
+//   })
+// });
+
+//update a post
+router.put('/:id', withAuth, (req, res) => {
+  Post.update ({
+
+  })
+})
+//delete a post
+router.delete('/:id', withAuth, (req, res) => {
   try {
-    const newProject = await Project.create({
-      ...req.body,
-      user_id: req.session.user_id,
-    });
-
-    res.status(200).json(newProject);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
-router.delete('/:id', async (req, res) => {
-  try {
-    const projectData = await Project.destroy({
+    const postData = await Post.destroy({
       where: {
         id: req.params.id,
-        user_id: req.session.user_id,
+    
       },
     });
 
-    if (!projectData) {
-      res.status(404).json({ message: 'No project found with this id!' });
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
       return;
     }
 
-    res.status(200).json(projectData);
+    res.status(200).json(postData);
   } catch (err) {
     res.status(500).json(err);
   }
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,19 @@
 const router = require('express').Router();
+const { Post, User,  Comment } = require('../models');
+const sequelize = require('../config/connection');
+
+//router.get('/', (req, res) => {
+
+// })
+
+// router.get('/login', (req, res) => {
+
+// })
+
+// router.get('/post/:id', (req, res) => {
+
+// })
+
+
 
 module.exports = router;
