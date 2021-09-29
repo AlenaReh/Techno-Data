58490eba3fd88dc40e10b4a47a2d8e98c0f4c1f0
Alena Rehberger <alenarehberger92@gmail.com>
2021-09-28 19:31:24 -0500

    Set up a structure for routes and changed one of the seeds files

--- server.js
+++ server.js
@@ -3,16 +3,15 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 const routes = require('./controllers');
+const app = express();
+const PORT = process.env.PORT || 3001;
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 // Inform Express.js which template engine we're using
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-const app = express();
-const PORT = process.env.PORT || 3001;
 
 const sess = {
   secret: 'Super secret secret',
