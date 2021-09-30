7b58a5ee9ffc7e5241cd4ab34666c34c788ac3bd
Alena Rehberger <alenarehberger92@gmail.com>
2021-09-30 06:37:21 -0500

    Created starter for dashboardRoutes

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,3 +1,30 @@
 const router = require('express').Router();
+const { Post, User,  Comment } = require('../models');
+const sequelize = require('../config/connection');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+
+    })
+});
+
+
+//should use the ID from the session
+router.get('', withAuth, (req, res) => {
+    Post.findAll({
+
+    })
+});
+
+//for user to be able to change/update their post
+router.get('', withAuth, (req, res) => {
+    Post.findeOne({
+
+    })
+});
+
+
+
 
 module.exports = router;
