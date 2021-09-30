25480b333a6b262f315859988da5bae9f05d3253
Alena Rehberger <alenarehberger92@gmail.com>
2021-09-30 06:58:52 -0500

    Working on home and user routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -17,6 +17,28 @@ router.get('/', (req, res) => {
 
 
 //GET users by id
+router.get('/:id', (req, res) => {
+  User.findOne({
+    attributes: { exclude: ['password']},
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Post,
+        attributes: ['id', 'title', 'text', 'date_created'],
+      },
+      {
+        model: Comment,
+        attributes: ['id', 'comment', 'date_created'],
+        include: {
+          model: Post,
+          attributes: ['title']
+        }
+      }
+    ]
+  })
+});
 
 
 
@@ -29,7 +51,7 @@ router.post('/', async (req, res) => {
 
     req.session.save(() => {
       req.session.user_id = userData.id;
-      req.session.loggedIn = true;
+      req.session.logged_in = true;
 
       res.status(200).json(userData);
     });
@@ -41,6 +63,7 @@ router.post('/', async (req, res) => {
 //login
 router.post('/login', async (req, res) => {
   try {
+    // Find the user who matches the posted e-mail address
     const userData = await User.findOne({ where: { email: req.body.email } });
 
     if (!userData) {
@@ -50,6 +73,7 @@ router.post('/login', async (req, res) => {
       return;
     }
 
+    // Verify the posted password with the password store in the database
     const validPassword = await userData.checkPassword(req.body.password);
 
     if (!validPassword) {
@@ -59,9 +83,10 @@ router.post('/login', async (req, res) => {
       return;
     }
 
+    // Create session variables based on the logged in user
     req.session.save(() => {
       req.session.user_id = userData.id;
-      req.session.loggedIn = true;
+      req.session.logged_in = true;
       
       res.json({ user: userData, message: 'You are now logged in!' });
     });
@@ -73,7 +98,8 @@ router.post('/login', async (req, res) => {
 
 //logout
 router.post('/logout', (req, res) => {
-  if (req.session.loggedIn) {
+  if (req.session.logged_in) {
+    // Remove the session variables
     req.session.destroy(() => {
       res.status(204).end();
     });
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,18 +1,50 @@
 const router = require('express').Router();
 const { Post, User,  Comment } = require('../models');
 const sequelize = require('../config/connection');
+const withAuth = require('../utils/auth');
 
-//router.get('/', (req, res) => {
 
-// })
+// Prevent non logged in users from viewing the homepage
+// router.get('/', withAuth, async (req, res) => {
+//     try {
+//       const userData = await User.findAll({
+//         attributes: { exclude: ['password'] },
+//       });
+  
+//       const users = userData.map((project) => project.get({ plain: true }));
+  
+//       res.render('homepage', {
+//         users,
+//         // Pass the logged in flag to the template
+//         logged_in: req.session.logged_in,
+//       });
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   });
 
-// router.get('/login', (req, res) => {
+router.get('/', (req, res) => {
+    console.log('====reqsession====', req.session);
+})
 
-// })
+
+
+router.get('/login', (req, res) => {
+  // If a session exists, redirect the request to the homepage
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
 
 // router.get('/post/:id', (req, res) => {
 
 // })
+
+//create a roter for users to be able to sign up
+
+
 
 
 
