d62ee72dd38c6cdf439365ee00ea9c8bf7ee50ea
Alena Rehberger <alenarehberger92@gmail.com>
2021-09-30 07:42:19 -0500

    Done with seeds

--- seeds/index.js
+++ seeds/index.js
@@ -1,1 +1,24 @@
 //Coordination of seeds files
+const sequelize = require('../config/connection');
+
+const userSeed = require('./userSeed');
+const postSeed = require('./postSeed');
+const commentSeed = require('./commentSeed');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+       
+    await userSeed();
+    console.log('users are SECCESSFULY seeded')
+
+    await postSeed();
+    console.log('posts are SECCESSFULY seeded')
+
+    await commentSeed();
+    console.log('comments are SECCESSFULY seeded')
+
+    process.exit(0);
+};
+
+seedDatabase();
+
