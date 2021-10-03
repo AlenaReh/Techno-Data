054ec2ad4c7adf5064ef21341d63d12a45c7510f
Alena Rehberger <alenarehberger92@gmail.com>
2021-10-02 19:14:15 -0500

    Done with post pouts

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -68,25 +68,43 @@ router.get('/id', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
-})
+});
 
 //create a new post
-// router.post('/', withAuth, (req, res) => {
-//   Post.create({
-//     title: req.body.title,
-//     text: req.body.text,
-//     user_id: req.body.user_id 
-//   })
-//     .then(postData => res.json(postData))
+router.post('/', withAuth, (req, res) => {
+  Post.create({
+    title: req.body.title,
+    text: req.body.text,
+    user_id: req.body.user_id 
+  })
+    .then(postData => res.json(postData))
 
-//     .catch(err => {
-//     res.status(400).json(err);
-//   })
-// });
+    .catch(err => {
+    res.status(400).json(err);
+  })
+});
 
 //update a post
 router.put('/:id', withAuth, (req, res) => {
   Post.update ({
+    title: req.body.title,
+    text: req.body.text
+  },
+  {
+    where: {
+    id: req.params.id
+    }
+  })
+  .then(postData => {
+    if (!postData) {
+      res.status(404).json({ message: 'No post found!!!' });
+      return;
+    }
+    res.json(postData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
 
   })
 })
@@ -96,15 +114,12 @@ router.delete('/:id', withAuth, async(req, res) => {
     const postData = await Post.destroy({
       where: {
         id: req.params.id,
-    
       },
     });
-
     if (!postData) {
       res.status(404).json({ message: 'No post found with this id!' });
       return;
     }
-
     res.status(200).json(postData);
   } catch (err) {
     res.status(500).json(err);
