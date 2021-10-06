ef943627008e64d52fa429f8378325ae19636602
Alena Rehberger <alenarehberger92@gmail.com>
2021-10-05 20:40:32 -0500

    Comments work now

--- public/js/commentPost.js
+++ public/js/commentPost.js
@@ -1,31 +1,32 @@
 async function commentFormHandler(event) {
+  event.preventDefault();
 
-    event.preventDefault();
-  
-    const comment = document.querySelector('textarea[name="comment-content"]').value.trim();
-    const post_id = window.location.toString().split('/')[
-      window.location.toString().split('/').length - 1
-    ];
-    if (comment) {
-        const response = await fetch('/api/comments', {
-          method: 'POST',
-          body: JSON.stringify({
-            post_id,
-            comment
-          }),
-          headers: {
-            'Content-Type': 'application/json'
-          }
+  const comment = document
+    .querySelector('textarea[name="comment"]')
+    .value.trim();
+  const post_id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
+  if (comment) {
+    const response = await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({
+        post_id,
+        comment,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-        });
-      
-        if (response.ok) {
-          document.location.reload();
-        } else {
-          alert(response.statusText);
-        }
-      }
-
+    if (response.ok) {
+      document.location.reload();
+    } else {
+      alert(response.statusText);
+    }
   }
-  
-  document.querySelector('.comment').addEventListener('submit', commentFormHandler);
+}
+
+document
+  .querySelector("#commentbtn")
+  .addEventListener("click", commentFormHandler);
