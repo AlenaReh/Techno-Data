28534501e6af20b648913d10967a8d3851e55c75
Alena Rehberger <alenarehberger92@gmail.com>
2021-10-07 14:38:23 -0500

    HEROKU deployment is braking my code, trying smth to fix it

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,7 @@
 console.log("Loding login JS file " )
 const loginFormHandler = async (event) => {
   event.preventDefault();
-  // alert("Button clicked"); 
+  // alert("Button clicked");
   const email = document.querySelector("#user-email").value.trim();
   const password = document.querySelector("#user-password").value.trim();
   console.log(email, password); 
@@ -21,3 +21,6 @@ const loginFormHandler = async (event) => {
 };
 console.log(document.getElementById("btnLogin"));
 document.getElementById("btnLogin").addEventListener("click", loginFormHandler);
+
+
+
